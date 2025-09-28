82ee5129bb4c01f3edb47d9e37069d34b81ee2f2
simonrydder <66967123+simonrydder@users.noreply.github.com>
2025-09-28 16:42:51 +0200

    feat: added functions to fetch data from lol api

---
+++ src/services/lol.py
@@ -0,0 +1,48 @@
+from enum import StrEnum
+
+import requests
+
+from models.lol import ChampionDragon, LolDataDragon
+
+BASE_URL = "https://ddragon.leagueoflegends.com/"
+
+
+class Language(StrEnum):
+    US = "en_US"
+    FR = "fr_FR"
+
+
+def fetch_versions() -> list[str]:
+    url = f"{BASE_URL}api/versions.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_latest_version(versions: list[str]) -> str:
+    return versions[0]
+
+
+def get_data_url(version: str, language: str) -> str:
+    url = f"{BASE_URL}cdn/{version}/data/{language}/champion.json"
+    return url
+
+
+def get_raw_champion_data(url: str) -> LolDataDragon:
+    response = requests.get(url)
+    response.raise_for_status()
+    res = response.json()
+    dd = LolDataDragon(**res)
+    return dd
+
+
+def get_champion_data(data_dragon: LolDataDragon) -> list[ChampionDragon]:
+    return list(data_dragon.data.values())
+
+
+def get_image_icon_url(image: str, version: str) -> str:
+    return f"{BASE_URL}cdn/{version}/img/champion/{image}"
+
+
+def get_loading_image_url(id: str) -> str:
+    return f"{BASE_URL}cdn/img/champion/loading/{id}_0.jpg"
